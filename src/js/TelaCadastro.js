141cfbfd7b5ec008a31e99523aa91f04c54e0769
João Menegati <joomenegati@gmail.com>
2019-12-01 20:08:24 -0300

    feature: Validar e exibir em tela campos do cadastro

--- src/js/TelaCadastro.js
+++ src/js/TelaCadastro.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+import { validate as ValidateCPF } from 'gerador-validador-cpf';
 import { router } from '../router';
 
 export default {
@@ -33,14 +34,16 @@ export default {
           'email': email,
           'password': password,
           'cpf': cpf,
-          'name': name,
+          'name': name.replace( "/[ãáàéèíìõóòúù]/gi", function (x) {
+            this.replaceRegex(x)
+          }),
           'birthDate': birthDate,
           'city': city,
           'district': district,
           'postalCode': postalCode,
           'street': street,
           'houseNumber': houseNumber,
-          'phone': phone
+          'phone': '55' + phone
         }, {
         "headers": {
           'Content-Type': 'application/json'
@@ -50,10 +53,130 @@ export default {
           router.push({ name: 'TelaLogin', params: { justRegistered: true } });
         })
         .catch(e => {
-          console.log(e);
+          console.log( e );
+          alert("Um ou mais campos inválidos!");
         });
+    },
+
+    replaceRegex( x ){
+      x = x.toLowerCase()
+
+      switch(x){
+        case 'ã':
+        case 'á':
+        case 'à':
+          return 'a'
+        case 'é':
+        case 'è':
+          return 'e'
+        case 'í':
+        case 'ì':
+          return 'i'
+        case 'õ':
+        case 'ó':
+        case 'ò':
+          return 'o'
+        case 'ú':
+        case 'ù':
+          return 'u'
+      }
     }
 
+  },
+
+  computed: {
+    //Estado dos campos dizendo se os dados presentes são válidos ou não
+
+    emailState() {
+      if (!this.form.email == ''){
+        var emailRegex = new RegExp('[^@]+@[^\.]+\..+', 'gi')
+        return emailRegex.test(this.form.email)
+      }
+
+      return null
+    },
+
+    passwordState(){
+      if(!this.form.password == '')
+        return this.form.password.length >= 4 && this.form.password.length <= 16 ? true : false
+
+      return null
+    },
+
+    passwordCheckState(){
+      if(!this.form.passwordCheck == '')
+        return this.form.password === this.form.passwordCheck  ? true : false
+
+      return null
+    },
+
+    cpfCheck(){
+      if(!this.form.cpf == ''){
+        var numberRegex = new RegExp('[0-9]')
+        return ValidateCPF(this.form.cpf) && numberRegex.test(this.form.cpf) ? true : false
+      }
+
+      return null
+    },
+
+    birthDateCheck() {
+      if (!this.form.birthDate == ''){
+        var birthYear = this.form.birthDate.substring(0, 4)
+        var currentYear = new Date();
+
+        return birthYear <= currentYear.getFullYear() - 12
+      }
+
+      return null
+    },
+
+    cityCheck(){
+      if(!this.form.city == '')
+        return this.form.city.length >= 4 ? true : false
+
+      return null
+    },
+
+    districtCheck() {
+      if(!this.form.district == '')
+        return this.form.district.length == 2 ? true : false
+      
+      return null
+    },
+
+    postalCodeCheck(){
+      if(!this.form.postalCode == ''){
+        var numberRegex = new RegExp('[0-9]')
+        return this.form.postalCode.length == 8 && numberRegex.test(this.form.postalCode) ? true : false
+      }
+      
+      return null
+    },
+
+    streetCheck(){
+      if(!this.form.street == '')
+        return this.form.street.length >= 4 ? true : false
+
+      return null
+    },
+
+    houserNumberCheck(){
+      if(!this.form.houseNumber == ''){
+        var numberRegex = new RegExp('[0-9]')
+        return numberRegex.test(this.form.houseNumber)
+      }
+
+      return null
+    },
+
+    phoneCheck(){
+      if(!this.form.phone == ''){
+        var numberRegex = new RegExp('[0-9]')
+        return numberRegex.test(this.form.phone) && this.form.phone.length == 11 ? true : false
+      }
+      
+      return null
+    }
   }
 
 }
