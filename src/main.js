6b2a476f41dfa9c39966540e90164a3b44643db6
João Menegati <joomenegati@gmail.com>
2019-12-01 20:08:24 -0300

    feature: Validar e exibir em a validação dos campos do cadastro

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import EventBus from './js/EventBus'
 import { router } from './router'
+import BoostrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BoostrapVue)
 
 Vue.config.productionTip = false
 
